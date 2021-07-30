f2941f23c862a0f345c8a72e39a187e230a517dd
Zouascur <zouascur@mail.ru>
2021-07-30 16:39:04 +0300

    add json

--- js/app.js
+++ js/app.js
@@ -1,5 +1,77 @@
 'use strict';
 
+/*
+    const dataJson =
+    {
+        "date": "5555 июля",
+        "usd": {
+        "buyRate": "73,75",
+            "sellRate": "75,24"
+    },
+        "eur": {
+        "buyRate": "86,70",
+            "sellRate": "88,74"
+    }
+    }
+*/
+//const localJson = JSON.stringify(dataJson);
+
+const xhr = new XMLHttpRequest();
+xhr.open('GET', 'http://localhost:9999/api/hw13');
+xhr.send();
+
+xhr.onload = function () {
+    //alert(`Загружено: ${xhr.status} ${xhr.responseText}`);
+
+    const dataRates = JSON.parse(xhr.response);
+    //console.log(dataRates);
+
+    const dateID = document.getElementById('date');
+    dateID.textContent = dataRates.date;
+    const usdBuyRate = document.getElementById('usd-buy-rate');
+    usdBuyRate.textContent = dataRates.usd.buyRate;
+    const usdSellRate = document.getElementById('usd-sell-rate');
+    usdSellRate.textContent = dataRates.usd.sellRate;
+    const eurBuyRate = document.getElementById('eur-buy-rate');
+    eurBuyRate.textContent = dataRates.eur.buyRate;
+    const eurSellRate = document.getElementById('eur-sell-rate');
+    eurSellRate.textContent = dataRates.eur.sellRate;
+
+
+};
+
+
+//xhr.onload = handleSuccess;
+/*
+function handleSuccess(evt) {
+
+    if (xhr.status!==200) {
+        const newDataJson = JSON.parse(evt.responseText);
+    }*/
+
+//const dataRates = JSON.parse();
+
+
+//}
+
+
+/*
+
+const dateID = document.getElementById('date');
+dateID.textContent = dataRates.date;
+const usdBuyRate = document.getElementById('usd-buy-rate');
+usdBuyRate.textContent = dataRates.usd.buyRate;
+const usdSellRate = document.getElementById('usd-sell-rate');
+usdSellRate.textContent = dataRates.usd.sellRate;
+const eurBuyRate = document.getElementById('eur-buy-rate');
+eurBuyRate.textContent = dataRates.eur.buyRate;
+const eurSellRate = document.getElementById('eur-sell-rate');
+eurSellRate.textContent = dataRates.eur.sellRate;
+
+}
+
+/*
+
 function updateExchangeRates(update) {
     const dateID = document.getElementById('date');
     dateID.textContent = update.date;
